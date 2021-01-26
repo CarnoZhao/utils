504ac2513a90b6da35f3324e4077359a9d34991b
CarnoZhao <273806108@qq.com>
2021-01-26 15:29:11 +0800

    convert to bn

--- model/lxchtan_net.py
+++ model/lxchtan_net.py
@@ -2,23 +2,14 @@ import numpy as np
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
-from torch.utils.data import Dataset
-import math
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=True)
 
-def conv1x1(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
-                     padding=0, bias=True)
-
-def conv2x2(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=2, stride=stride,
-                     padding=1, bias=True)
+def convbn(in_planes, out_planes):
+    return conv3x3(in_planes, out_planes), nn.BatchNorm2d(out_planes), nn.LeakyReLU()
 
 # create your own CREncoder
 class CREncoder(nn.Module):
@@ -30,11 +21,12 @@ class CREncoder(nn.Module):
         self.conv2 = conv3x3(2, 2)
         self.fc = nn.Linear(768, int(feedback_bits / self.B))
         self.sig = nn.Sigmoid()
+        self.relu = nn.LeakyReLU()
         # self.quantize = QuantizationLayer(self.B)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = F.relu(self.conv2(out))
+        out = self.relu(self.conv1(x))
+        out = self.relu(self.conv2(out))
         out = out.view(-1, 768)
         out = self.fc(out)
         out = self.sig(out)
@@ -52,10 +44,10 @@ class CRDecoder(nn.Module):
         self.feedback_bits = feedback_bits
         # self.dequantize = DequantizationLayer(self.B)
 
-        self.conv2nums = 2
-        self.conv3nums = 3
+        self.conv2nums = 5
+        self.conv3nums = 5
         self.conv4nums = 5
-        self.conv5nums = 3
+        self.conv5nums = 5
 
         self.multiConvs2 = nn.ModuleList()
         self.multiConvs3 = nn.ModuleList()
@@ -67,81 +59,39 @@ class CRDecoder(nn.Module):
         self.sig = nn.Sigmoid()
 
         self.multiConvs2.append(nn.Sequential(
-                conv3x3(2, 64),
-                nn.BatchNorm2d(64),
-                nn.ReLU(),
-                conv3x3(64, 256),
-                nn.BatchNorm2d(256),
-                nn.ReLU()))
+                *convbn(2, 64),
+                *convbn(64, 64)))
         self.multiConvs3.append(nn.Sequential(
-                conv3x3(256, 512),
-                nn.BatchNorm2d(512),
-                nn.ReLU(),
-                conv3x3(512, 512),
-                nn.BatchNorm2d(512),
-                nn.ReLU()))
+                *convbn(64, 128),
+                *convbn(128, 128)))
         self.multiConvs4.append(nn.Sequential(
-                conv3x3(512, 1024),
-                nn.BatchNorm2d(1024),
-                nn.ReLU(),
-                conv3x3(1024, 1024),
-                nn.BatchNorm2d(1024),
-                nn.ReLU()))
+                *convbn(128, 256),
+                *convbn(256, 256)))
         self.multiConvs5.append(nn.Sequential(
-                conv3x3(1024, 128),
-                nn.BatchNorm2d(128),
-                nn.ReLU(),
-                conv3x3(128, 32),
-                nn.BatchNorm2d(32),
-                nn.ReLU(),
-                conv3x3(32, 2),
-                nn.BatchNorm2d(2),
-                nn.ReLU()))
+                *convbn(256, 128),
+                *convbn(128, 32),
+                *convbn(32, 2)))
                 
         for _ in range(self.conv2nums):
             self.multiConvs2.append(nn.Sequential(
-                conv3x3(256, 64),
-                nn.BatchNorm2d(64),
-                nn.ReLU(),
-                conv3x3(64, 64),
-                nn.BatchNorm2d(64),
-                nn.ReLU(),
-                conv3x3(64, 256),
-                nn.BatchNorm2d(256),
-                nn.ReLU()))
+                *convbn(64, 64),
+                *convbn(64, 64),
+                *convbn(64, 64)))
         for _ in range(self.conv3nums):
             self.multiConvs3.append(nn.Sequential(
-                conv3x3(512, 128),
-                nn.BatchNorm2d(128),
-                nn.ReLU(),
-                conv3x3(128, 128),
-                nn.BatchNorm2d(128),
-                nn.ReLU(),
-                conv3x3(128, 512),
-                nn.BatchNorm2d(512),
-                nn.ReLU()))
+                *convbn(128, 128),
+                *convbn(128, 128),
+                *convbn(128, 128)))
         for _ in range(self.conv4nums):
             self.multiConvs4.append(nn.Sequential(
-                conv3x3(1024, 256),
-                nn.BatchNorm2d(256),
-                nn.ReLU(),
-                conv3x3(256, 256),
-                nn.BatchNorm2d(256),
-                nn.ReLU(),
-                conv3x3(256, 1024),
-                nn.BatchNorm2d(1024),
-                nn.ReLU()))
+                *convbn(256, 256),
+                *convbn(256, 256),
+                *convbn(256, 256)))
         for _ in range(self.conv5nums):
             self.multiConvs5.append(nn.Sequential(
-                conv3x3(2, 32),
-                nn.BatchNorm2d(32),
-                nn.ReLU(),
-                conv3x3(32, 32),
-                nn.BatchNorm2d(32),
-                nn.ReLU(),
-                conv3x3(32, 2),
-                nn.BatchNorm2d(2),
-                nn.ReLU()))
+                *convbn(2, 32),
+                *convbn(32, 32),
+                *convbn(32, 2)))
 
     def forward(self, x):
         # out = self.dequantize(x)
@@ -177,3 +127,16 @@ class CRDecoder(nn.Module):
         out = self.out_cov(out)
         # out = self.sig(out)
         return out
+
+if __name__ == "__main__":
+    from torchviz import make_dot
+    e = CREncoder(400)
+    d = CRDecoder(400)
+    a = torch.randn(10, 2, 24, 16)
+    b = e(a)
+    make_dot(b, dict(e.named_parameters())).view("encoder", cleanup = True)
+    pass
+    b = torch.tensor(b.detach().numpy())
+    c = d(b)
+    make_dot(c, dict(d.named_parameters())).view("decoder", cleanup = True)
+    pass
