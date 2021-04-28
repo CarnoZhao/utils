cd594fc85ab0b524e822bf674e176d17a035535e
CarnoZhao <273806108@qq.com>
2021-04-28 17:15:23 +0800

    add bounary suface loss

--- seg/bound.py
+++ seg/bound.py
@@ -1,79 +1,107 @@
+#!/usr/env/bin python3.9
+
+from typing import List, cast
+
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import numpy as np
+from torch import Tensor, einsum
+from typing import Any, Callable, Iterable, List, Set, Tuple, TypeVar, Union, cast
+from torchvision import transforms
+from functools import partial, reduce
+from operator import itemgetter, mul
+from scipy.ndimage import distance_transform_edt as eucl_distance
+from PIL import Image, ImageOps
+
+# from utils import simplex, probs2one_hot, one_hot
+# from utils import one_hot2hd_dist
+
+D = Union[Image.Image, np.ndarray, Tensor]
+
+def uniq(a: Tensor) -> Set:
+    return set(torch.unique(a.cpu()).numpy())
 
 
-def one_hot(label, n_classes, requires_grad=True):
-    """Return One Hot Label"""
-    divce = label.device
-    one_hot_label = torch.eye(
-        n_classes, device="cuda", requires_grad=requires_grad)[label]
-    one_hot_label = one_hot_label.transpose(1, 3).transpose(2, 3)
+def sset(a: Tensor, sub: Iterable) -> bool:
+    return uniq(a).issubset(sub)
 
-    return one_hot_label
+def simplex(t: Tensor, axis=1) -> bool:
+    _sum = cast(Tensor, t.sum(axis).type(torch.float32))
+    _ones = torch.ones_like(_sum, dtype=torch.float32)
+    return torch.allclose(_sum, _ones)
 
 
-class BoundaryLoss(nn.Module):
-    """Boundary Loss proposed in:
-    Alexey Bokhovkin et al., Boundary Loss for Remote Sensing Imagery Semantic Segmentation
-    https://arxiv.org/abs/1905.07852
-    """
+def one_hot(t: Tensor, axis=1) -> bool:
+    return simplex(t, axis) and sset(t, [0, 1])
 
-    def __init__(self, theta0=3, theta=5):
-        super().__init__()
+def class2one_hot(seg: Tensor, K: int) -> Tensor:
+    # Breaking change but otherwise can't deal with both 2d and 3d
+    # if len(seg.shape) == 3:  # Only w, h, d, used by the dataloader
+    #     return class2one_hot(seg.unsqueeze(dim=0), K)[0]
 
-        self.theta0 = theta0
-        self.theta = theta
+    assert sset(seg, list(range(K))), (uniq(seg), K)
 
-    def forward(self, pred, gt):
-        """
-        Input:
-            - pred: the output from model (before softmax)
-                    shape (N, C, H, W)
-            - gt: ground truth map
-                    shape (N, H, w)
-        Return:
-            - boundary loss, averaged over mini-bathc
-        """
+    b, *img_shape = seg.shape 
 
-        n, c, _, _ = pred.shape
+    device = seg.device
+    res = torch.zeros((b, K, *img_shape), dtype=torch.int32, device=device).scatter_(1, seg[:, None, ...], 1)
 
-        # softmax so that predicted map can be distributed in [0, 1]
-        pred = torch.softmax(pred, dim=1)
+    assert res.shape == (b, K, *img_shape)
+    assert one_hot(res)
 
-        # one-hot vector of ground truth
-        one_hot_gt = one_hot(gt, c)
+    return res
 
-        # boundary map
-        gt_b = F.max_pool2d(
-            1 - one_hot_gt, kernel_size=self.theta0, stride=1, padding=(self.theta0 - 1) // 2)
-        gt_b -= 1 - one_hot_gt
+def one_hot2dist(seg: np.ndarray, resolution: Tuple[float, float, float] = None,
+                 dtype=None) -> np.ndarray:
+    assert one_hot(torch.tensor(seg), axis=0)
+    K: int = len(seg)
 
-        pred_b = F.max_pool2d(
-            1 - pred, kernel_size=self.theta0, stride=1, padding=(self.theta0 - 1) // 2)
-        pred_b -= 1 - pred
+    res = np.zeros_like(seg, dtype=dtype)
+    for k in range(K):
+        posmask = seg[k].astype(np.bool)
 
-        # extended boundary map
-        gt_b_ext = F.max_pool2d(
-            gt_b, kernel_size=self.theta, stride=1, padding=(self.theta - 1) // 2)
+        if posmask.any():
+            negmask = ~posmask
+            res[k] = eucl_distance(negmask, sampling=resolution) * negmask \
+                - (eucl_distance(posmask, sampling=resolution) - 1) * posmask
+        # The idea is to leave blank the negative classes
+        # since this is one-hot encoded, another class will supervise that pixel
 
-        pred_b_ext = F.max_pool2d(
-            pred_b, kernel_size=self.theta, stride=1, padding=(self.theta - 1) // 2)
+    return res
 
-        # reshape
-        gt_b = gt_b.view(n, c, -1)
-        pred_b = pred_b.view(n, c, -1)
-        gt_b_ext = gt_b_ext.view(n, c, -1)
-        pred_b_ext = pred_b_ext.view(n, c, -1)
+def gt_transform(resolution: Tuple[float, ...], K: int) -> Callable[[D], Tensor]:
+    return transforms.Compose([
+            lambda img: np.array(img)[...],
+            lambda nd: torch.tensor(nd, dtype=torch.int64)[None, ...],  # Add one dimension to simulate batch
+            partial(class2one_hot, K=K),
+            itemgetter(0)  # Then pop the element to go back to img shape
+    ])
 
-        # Precision, Recall
-        P = torch.sum(pred_b * gt_b_ext, dim=2) / (torch.sum(pred_b, dim=2) + 1e-7)
-        R = torch.sum(pred_b_ext * gt_b, dim=2) / (torch.sum(gt_b, dim=2) + 1e-7)
+def dist_map_transform(resolution: Tuple[float, ...], K: int) -> Callable[[D], Tensor]:
+    return transforms.Compose([
+            gt_transform(resolution, K),
+            lambda t: t.cpu().numpy(),
+            partial(one_hot2dist, resolution=resolution),
+            lambda nd: torch.tensor(nd, dtype=torch.float32)
+    ])
 
-        # Boundary F1 Score
-        BF1 = 2 * P * R / (P + R + 1e-7)
+class SurfaceLoss():
+    def __init__(self, **kwargs):
+        # Self.idc is used to filter out some classes of the target mask. Use fancy indexing
+        self.idc: List[int] = kwargs["idc"]
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
 
-        # summing BF1 Score for each class and average over mini-batch
-        loss = torch.mean(1 - BF1)
+    def __call__(self, probs: Tensor, dist_maps: Tensor) -> Tensor:
+        assert simplex(probs)
+        assert not one_hot(dist_maps)
+
+        pc = probs[:, self.idc, ...].type(torch.float32)
+        dc = dist_maps[:, self.idc, ...].type(torch.float32)
+
+        multipled = einsum("bkwh,bkwh->bkwh", pc, dc)
+
+        loss = multipled.mean()
 
         return loss
+
+
+BoundaryLoss = SurfaceLoss
