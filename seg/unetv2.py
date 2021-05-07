ec2ed4e1e0d23fb2cd0232ab219bda9fa839aff8
CarnoZhao <273806108@qq.com>
2021-05-07 13:17:37 +0800

    add effv2s unet, with timm 0.4.8

--- seg/unetv2.py
+++ seg/unetv2.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 
 import timm
 from timm.models.layers import ConvBnAct, get_act_layer, create_act_layer, create_attn, create_conv2d
+import torch.utils.model_zoo as model_zoo
 
 import segmentation_models_pytorch as smp
 import segmentation_models_pytorch.base.modules as md
@@ -13,6 +14,132 @@ import segmentation_models_pytorch.base.modules as md
 from ..acti.frelu import FReLU
 from ..acti.dyrelu import DyReLUA, DyReLUB, DyReLUC
 
+from timm.models.efficientnet import EfficientNet
+from timm.models.efficientnet import decode_arch_def, round_channels, default_cfgs
+from timm.models.layers.activations import Swish
+from timm.models.efficientnet_blocks import round_channels, resolve_bn_args, resolve_act_layer, BN_EPS_TF_DEFAULT
+
+from segmentation_models_pytorch.encoders._base import EncoderMixin
+
+def get_efficientnet_v2_kwargs(channel_multiplier=1.0, depth_multiplier=1.0):
+    """ Creates an EfficientNet-V2s model
+    NOTE: this is a preliminary definition based on paper, awaiting official code release for details
+    and weights
+    Ref impl:
+    Paper: `EfficientNetV2: Smaller Models and Faster Training` - https://arxiv.org/abs/2104.00298
+    """
+
+    arch_def = [
+        # FIXME it's not clear if the FusedMBConv layers have SE enabled for the Small variant,
+        # Table 4 suggests no. 23.94M params w/o, 23.98 with which is closer to 24M.
+        # ['er_r2_k3_s1_e1_c24_se0.25'],
+        # ['er_r4_k3_s2_e4_c48_se0.25'],
+        # ['er_r4_k3_s2_e4_c64_se0.25'],
+        ['er_r2_k3_s1_e1_c24'],
+        ['er_r4_k3_s2_e4_c48'],
+        ['er_r4_k3_s2_e4_c64'],
+        ['ir_r6_k3_s2_e4_c128_se0.25'],
+        ['ir_r9_k3_s1_e6_c160_se0.25'],
+        ['ir_r15_k3_s2_e6_c272_se0.25'],
+    ]
+    model_kwargs = dict(
+        block_args=decode_arch_def(arch_def, depth_multiplier),
+        num_features=round_channels(1792, channel_multiplier, 8, None),
+        stem_size=24,
+        channel_multiplier=channel_multiplier,
+        norm_kwargs={},
+        act_layer=Swish,  # FIXME this is an assumption, paper does not mention
+        drop_rate=0.2,
+        drop_path_rate=0.2,
+    )
+    return model_kwargs
+
+class EfficientNetV2Encoder(EfficientNet, EncoderMixin):
+
+    def __init__(self, stage_idxs, out_channels, depth=5, channel_multiplier=1.0, depth_multiplier=1.0):
+            kwargs = get_efficientnet_v2_kwargs(channel_multiplier, depth_multiplier)
+            super().__init__(**kwargs)
+
+            self._stage_idxs = stage_idxs
+            self._out_channels = out_channels
+            self._depth = depth
+            self._in_channels = 3
+
+            del self.classifier
+
+    def get_stages(self):
+        return [
+            nn.Identity(),
+            nn.Sequential(self.conv_stem, self.bn1, self.act1),
+            self.blocks[:self._stage_idxs[0]],
+            self.blocks[self._stage_idxs[0]:self._stage_idxs[1]],
+            self.blocks[self._stage_idxs[1]:self._stage_idxs[2]],
+            self.blocks[self._stage_idxs[2]:],
+        ]
+
+    def forward(self, x):
+        stages = self.get_stages()
+
+        features = []
+        for i in range(self._depth + 1):
+            x = stages[i](x)
+            features.append(x)
+
+        return features
+
+    def load_state_dict(self, state_dict, **kwargs):
+        state_dict.pop("classifier.bias")
+        state_dict.pop("classifier.weight")
+        super().load_state_dict(state_dict, **kwargs)
+
+def prepare_settings(settings):
+    return {
+        "mean": settings["mean"],
+        "std": settings["std"],
+        "url": settings["url"],
+        "input_range": (0, 1),
+        "input_space": "RGB",
+    }
+
+encoders = {
+
+    "timm-efficientnet-v2s": {
+        "encoder": EfficientNetV2Encoder,
+        "pretrained_settings": {
+            "imagenet": prepare_settings(default_cfgs["efficientnet_v2s"]),
+        },
+        "params": {
+            "out_channels": (3, 24, 48, 64, 160, 272),
+            "stage_idxs": (2, 3, 5),
+            "channel_multiplier": 1.0,
+            "depth_multiplier": 1.0,
+        },
+    },
+}
+
+def get_encoder_effv2(name, in_channels=3, depth=5, weights=None):
+
+    try:
+        Encoder = encoders[name]["encoder"]
+    except KeyError:
+        raise KeyError("Wrong encoder name `{}`, supported encoders: {}".format(name, list(encoders.keys())))
+
+    params = encoders[name]["params"]
+    params.update(depth=depth)
+    encoder = Encoder(**params)
+
+    if weights is not None:
+        try:
+            settings = encoders[name]["pretrained_settings"][weights]
+        except KeyError:
+            raise KeyError("Wrong pretrained weights `{}` for encoder `{}`. Avaliable options are: {}".format(
+                weights, name, list(encoders[name]["pretrained_settings"].keys()),
+            ))
+        encoder.load_state_dict(model_zoo.load_url(settings["url"]))
+
+    encoder.set_in_channels(in_channels)
+
+    return encoder
 
 class Conv2dAct(nn.Sequential):
     def __init__(
@@ -141,6 +268,96 @@ class UnetDecoderV2(nn.Module):
             x = decoder_block(x, skip)
 
         return x
+
+class UnetEffV2(smp.base.SegmentationModel):
+    """Unet_ is a fully convolution neural network for image semantic segmentation. Consist of *encoder* 
+    and *decoder* parts connected with *skip connections*. Encoder extract features of different spatial 
+    resolution (skip connections) which are used by decoder to define accurate segmentation mask. Use *concatenation*
+    for fusing decoder blocks with skip connections.
+
+    Args:
+        encoder_name: Name of the classification model that will be used as an encoder (a.k.a backbone)
+            to extract features of different spatial resolution
+        encoder_depth: A number of stages used in encoder in range [3, 5]. Each stage generate features 
+            two times smaller in spatial dimentions than previous one (e.g. for depth 0 we will have features 
+            with shapes [(N, C, H, W),], for depth 1 - [(N, C, H, W), (N, C, H // 2, W // 2)] and so on).
+            Default is 5
+        encoder_weights: One of **None** (random initialization), **"imagenet"** (pre-training on ImageNet) and 
+            other pretrained weights (see table with available weights for each encoder_name)
+        decoder_channels: List of integers which specify **in_channels** parameter for convolutions used in decoder.
+            Lenght of the list should be the same as **encoder_depth**
+        decoder_use_batchnorm: If **True**, BatchNorm2d layer between Conv2D and Activation layers
+            is used. If **"inplace"** InplaceABN will be used, allows to decrease memory consumption.
+            Avaliable options are **True, False, "inplace"**
+        decoder_attention_type: Attention module used in decoder of the model. Avaliable options are **None** and **scse**.
+            SCSE paper - https://arxiv.org/abs/1808.08127
+        in_channels: A number of input channels for the model, default is 3 (RGB images)
+        classes: A number of classes for output mask (or you can think as a number of channels of output mask)
+        activation: An activation function to apply after the final convolution layer.
+            Avaliable options are **"sigmoid"**, **"softmax"**, **"logsoftmax"**, **"identity"**, **callable** and **None**.
+            Default is **None**
+        aux_params: Dictionary with parameters of the auxiliary output (classification head). Auxiliary output is build 
+            on top of encoder if **aux_params** is not **None** (default). Supported params:
+                - classes (int): A number of classes
+                - pooling (str): One of "max", "avg". Default is "avg"
+                - dropout (float): Dropout factor in [0, 1)
+                - activation (str): An activation function to apply "sigmoid"/"softmax" (could be **None** to return logits)
+
+    Returns:
+        ``torch.nn.Module``: Unet
+
+    .. _Unet:
+        https://arxiv.org/abs/1505.04597
+
+    """
+
+    def __init__(
+        self,
+        encoder_name: str = "timm-efficientnet-v2s",
+        encoder_depth: int = 5,
+        encoder_weights: Optional[str] = "imagenet",
+        decoder_use_batchnorm: bool = True,
+        decoder_channels: List[int] = (256, 128, 64, 32, 16),
+        decoder_attention_type: Optional[str] = None,
+        in_channels: int = 3,
+        classes: int = 1,
+        activation: Optional[Union[str, callable]] = None,
+        aux_params: Optional[dict] = None,
+    ):
+        super().__init__()
+
+        self.encoder = get_encoder_effv2(
+            encoder_name,
+            in_channels=in_channels,
+            depth=encoder_depth,
+            weights=encoder_weights,
+        )
+
+        self.decoder = smp.unet.decoder.UnetDecoder(
+            encoder_channels=self.encoder.out_channels,
+            decoder_channels=decoder_channels,
+            n_blocks=encoder_depth,
+            use_batchnorm=decoder_use_batchnorm,
+            center=True if encoder_name.startswith("vgg") else False,
+            attention_type=decoder_attention_type,
+        )
+
+        self.segmentation_head = smp.base.SegmentationHead(
+            in_channels=decoder_channels[-1],
+            out_channels=classes,
+            activation=activation,
+            kernel_size=3,
+        )
+
+        if aux_params is not None:
+            self.classification_head = smp.base.ClassificationHead(
+                in_channels=self.encoder.out_channels[-1], **aux_params
+            )
+        else:
+            self.classification_head = None
+
+        self.name = "u-{}".format(encoder_name)
+        self.initialize()
 
 class UnetV2(smp.base.SegmentationModel):
     """Unet_ is a fully convolution neural network for image semantic segmentation. Consist of *encoder* 
